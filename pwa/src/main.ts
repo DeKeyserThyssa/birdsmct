f2741891ce495ced91a8092142c95b9bcde7f790
Thyssa De Keyser <thyssa.de.keyser@student.howest.be>
2022-09-19 16:34:34 +0200

    feat: labo-01-intro

--- pwa/src/main.ts
+++ pwa/src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
+import { App as VueApp, createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import '@unocss/reset/tailwind.css'
+import 'uno.css'
+
+const app: VueApp = createApp(App)
+
+app.mount('#app')
