01d0998b6a0a5cf696139c2d7aa4cf8e2cbfa0e1
DeKeyserThyssa <thyssa.de.keyser@student.howest.be>
2022-11-18 19:59:42 +0100

    add some dark mode

--- pwa/cypress/e2e/1-auth-flow/register.cy.ts
+++ pwa/cypress/e2e/1-auth-flow/register.cy.ts
@@ -1,3 +1,36 @@
+import {
+  Auth,
+  connectAuthEmulator,
+  initializeAuth,
+  signInWithEmailAndPassword,
+  UserCredential,
+  indexedDBLocalPersistence,
+} from 'firebase/auth';
+
+function getAuthEmulatorHost() {
+  const host = Cypress.env('NEXT_PUBLIC_FIREBASE_EMULATOR_HOST') as string;
+  const port = Cypress.env('NEXT_PUBLIC_FIREBASE_AUTH_EMULATOR_PORT') as string;
+
+  return ['http://', host, ':', port].join('');
+}
+
+let auth: Auth;
+
+function getAuth() {
+  const app = createFirebaseApp();
+
+  auth =
+    auth ||
+    initializeAuth(app, {
+      persistence: indexedDBLocalPersistence,
+    });
+
+  connectAuthEmulator(auth, getAuthEmulatorHost());
+
+  return auth;
+}
+
+
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:5173/auth/register')
